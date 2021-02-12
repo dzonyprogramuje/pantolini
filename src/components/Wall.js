c574e3f8d285d1680a46cc0bdbe07d454de97e30
dzonyprogramuje <dzonyprogramuje@gmail.com>
2021-02-12 12:35:23 +0100

    Wall changed a little bit. New Icons added and Comments-Feld is going to open

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -9,28 +9,41 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+
 import Typography from '@material-ui/core/Typography';
 
 import InputAdornment from '@material-ui/core/InputAdornment';
 
 import Divider from '@material-ui/core/Divider';
-
+import Collapse from '@material-ui/core/Collapse';
+import CardContent from '@material-ui/core/CardContent'
 import {users} from './Users'
 
 const useStyles = makeStyles((theme) => ({
-
-  small: {
-    width: theme.spacing(3),
-    height: theme.spacing(3),
-  },
-
+    button:{
+        marginLeft: theme.spacing(2)
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+    },
 }));
 
 export default function Wall() {
     const classes = useStyles();
-    let comments = []
+    const [expand, setExpand] = useState('')
+
+    const handleExpand = (index)=>{ 
+        if (expand === index) {
+        setExpand("")
+        } else {
+        setExpand(index)     
+    }
+  }    
 
   return (
+      
     <Container maxWidth="md">
         <div className={classes.root}>
         <Grid container spacing={3}>            
@@ -41,11 +54,12 @@ export default function Wall() {
             </Grid>
         {
         users.map(user =>{
-            return(
-                user.posts.map(post => {                            
+            return(                
+                user.posts.map((post,index) => {  
+                    
                     return(
                         <Grid item xs={12}>
-                            <Paper elevation={3}>
+                            <Paper elevation={3} >
                                 <Box p={4}>
                                     <Grid item xs={12}>
                                         <Box display="flex" alignItems="center">
@@ -62,26 +76,38 @@ export default function Wall() {
                                             {post.description}
                                             </Typography>
                                         </Box>
-                                        <Box disableSpacing flexGrow={1}>
-                                            <IconButton disableSpacing >
-                                                <ThumbUpAltIcon   disableSpacing  />
-                                            </IconButton>
-                                                
-                                        </Box>                                                
+                                               
                                         <Grid container my={3} justify='space-between' direction='col'>                                                    
-                                            <Box><Button size="small">Comments ({post.comments.length})</Button></Box>  
-                                            <Box><Button size="small">Show more</Button></Box>  
+                                            <Box><Button size="small" onClick={()=>handleExpand(index)}>Comments ({post.comments.length})</Button></Box>  
+                                            <Box>
+                                                <Button
+                                                    variant="contained"
+                                                    color="secondary"
+                                                    size="small"
+                                                    className={classes.button}
+                                                    startIcon={<ThumbDownIcon />}
+                                                >hate It</Button> 
+                                                <Button
+                                                    variant="contained"
+                                                    color="primary"
+                                                    size="small"
+                                                    className={classes.button}
+                                                    startIcon={<ThumbUpAltIcon />}
+                                                >Love It</Button>                                          
+                                            </Box>                                             
                                         </Grid> 
                                         <Box my={2} flexGrow={1}>
                                                 <Divider />   
                                         </Box>
-                                        <Box flexGrow={1}>
-                                            <Grid container justify='space-between' direction='row' alignItems="stretch" my={3}>
+                                        <Box flexGrow={1}>                                            
+                                            <Grid container justify='space-between' direction='row' alignItems="stretch" spacing={3}>
                                                 <Grid item xs={12}>
-                                                    <TextField id="standard-basic" variant="filled" label="Comment this post hiere" fullWidth />                                                    
+                                                    <TextField id="standard-basic" variant="outlined" label="Comment this post hiere" fullWidth className={classes.input}/>                                                    
                                                 </Grid>
                                             </Grid>                                
                                         </Box>
+                                        <Collapse in={index === expand} timeout="auto" unmountOnExit>
+                                            <CardContent>
                                         <Box flexGrow={1}>
                                             {
                                             post.comments.map(comment => {
@@ -89,7 +115,7 @@ export default function Wall() {
                                                 <Grid container justify='space-between' direction='row' alignItems="stretch" m={2}>
                                                     <Grid item xs={11}>
                                                         <Box my={2} mr={3}>
-                                                            <Paper > 
+                                                            <Paper variant='outlined'> 
                                                                 <Box p={3}>
                                                                     <Typography variant="body2" display="block" gutterBottom>
                                                                         <b>{comment.author}:</b>
@@ -112,8 +138,11 @@ export default function Wall() {
                                                 </Grid>                                                        
                                                 )
                                                 })                                                                                                                   
-                                            }                                             
-                                        </Box>                                                                            
+                                            }  
+                                        </Box>   
+                                        </CardContent>
+                                            </Collapse>                                             
+                                                                                                               
                                     </Grid>
                                 </Box>                                
                             </Paper>
